3e9138278c24f03f46dc58f4db08c1f3af4ded0a
BeeKpr7 <68864589+BeeKpr7@users.noreply.github.com>
2023-02-01 15:25:04 +0100

    Breeze-React (#1)
    * React OK
    
    * Breeze OK
    
    * add logo world
    
    * IMG load OK

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Example() {
+    return (
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('example')) {
+    const Index = ReactDOM.createRoot(document.getElementById("example"));
+
+    Index.render(
+        <React.StrictMode>
+            <Example/>
+        </React.StrictMode>
+    )
+}
---
+++ resources/js/components/WorldLogo.jsx
@@ -0,0 +1,9 @@
+export default function WorldLogo({ className }) {
+    return (
+        <img
+            className={className}
+            src="/images/logo/wolrd-color-futurist.png"
+            alt="world-wide-web"
+        />
+    );
+}
